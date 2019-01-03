a12dbbab0d45bb8f92f3968ae2e67542d0b86e12
Patrick Lang <plang@microsoft.com>
2019-01-03 12:36:20 -0800

    Adding flannel to deployment

--- setup-master.sh
+++ setup-master.sh
@@ -2,6 +2,9 @@ export cidr='10.244.0.0/16'
 export masterip=`ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1`
 
 kubeadm init --pod-network-cidr=$cidr
+
+kubectl apply -f /vagrant/kube-flannel-hostgw.yml
+
 export digest=`openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | openssl dgst -sha256 -hex | sed 's/^.* //'`
 export token=`kubeadm token list | grep "default bootstrap" | awk '{print $1}'`
 
